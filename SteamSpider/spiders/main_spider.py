06ab857b3949447e01ecad6c6ae4a3f2ab2dd4bf
Igor <igor_ryabkov_1@mail.com>
2022-12-06 03:43:53 +0300

    Initial-commit

---
+++ SteamSpider/spiders/main_spider.py
@@ -0,0 +1,42 @@
+import scrapy
+import re
+
+# 1) Вытянуть платформы
+# 2) Вытянуть разные ссылки
+# 3) Отличающиеся страницы
+
+class SteamSpider(scrapy.Spider):
+    name = 'little_spiderok'
+    pages = 5  # at least 1
+    link = 'https://store.steampowered.com/search/?sort_by=_ASC&sort_order=0&term=%D0%B8%D0%BD%D0%B4%D0%B8&supportedlang=russian&page='
+    link = 'https://store.steampowered.com/search/?sort_by=&sort_order=0&term=minecraft&supportedlang=russian&page='
+    link = 'https://store.steampowered.com/search/?sort_by=&sort_order=0&term=clickers&supportedlang=russian&page='
+    start_urls = [link + '1']
+
+    def parse(self, response):
+        for l in response.xpath('//a[@data-gpnav="item"]/@href'):
+            yield response.follow(l, callback=self.parse_page)
+
+        for i in range(2, SteamSpider.pages + 1):
+            yield response.follow(SteamSpider.link + str(i), callback=self.parse)
+
+    def parse_page(self, response):
+        to_path = []
+        for i in response.xpath('//div[@class="blockbg"]/a/text()'):
+            to_path.append(i.get())
+        to_path = to_path[1:]
+        data = response.xpath('//div[@class="date"]/text()').get()
+        if data and int(data.split()[2]) <= 2000:
+            return
+        yield {
+            'title': response.xpath('//div[@id="appHubAppName_responsive"]/text()').get(),
+            'path': '/'.join(to_path),
+            'reviews': ' '.join([x.get() for x in response.xpath('//div[@class="summary_section"]/span/text()')]),
+            'release date': data,
+            'developer': response.xpath('//div[@id="developers_list"]/a/text()').get(),
+            'popular tags': ', '.join(
+                [x.get().strip() for x in response.xpath('//div[@class="glance_tags popular_tags"]/a/text()')]),
+            'price':  response.xpath('//div[@class="discount_final_price"]/text() | //div[@class="game_purchase_price price"]/text()').get().strip(),
+            'platforms available': ', '.join([x.get().strip() for x in response.xpath('//div[@class="sysreq_contents"]/div/@data-os')])
+        }
+# response.xpath('//a[@data-gpnav="item"]/@href')[20].get()
